8c762d11cfeb709500779bfccce4e90858c9eaa4
shmul95 <samuel.gross@epitech.eu>
2026-03-03 12:43:55 +0100

    fix(copy): can now copy in nvim, amazing right ?

--- nix/config.nix
+++ nix/config.nix
@@ -4,8 +4,20 @@
     lsp.enable = true;
     lazy.enable = true;
 
+    clipboard = {
+       enable = true;
+       providers.wl-copy.enable = true;
+       registers = "unnamedplus";
+    };
+
+    globals = {
+      mapleader = " "; # space
+      shiftwidth = 4; # but 2 for yml, nix, etc
+    };
+
     extraLuaFiles = [
       ./lua/keymap.lua
     ];
+
   };
 }
