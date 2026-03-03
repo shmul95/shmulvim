8d7e8c4a21f7665947e3cec1f17b9885f1756dca
shmul95 <samuel.gross@epitech.eu>
2026-03-03 12:43:35 +0100

    fix(theme): remove black background because it doesnt always work

--- nix/theme.nix
+++ nix/theme.nix
@@ -8,9 +8,9 @@
       vim.g.dracula_colorterm = 0
       vim.g.dracula_italic = true
 
-      vim.api.nvim_set_hl(0, "Normal", { bg = "#000000" })
-      vim.api.nvim_set_hl(0, "NormalNC", { bg = "#000000" })
-      vim.api.nvim_set_hl(0, "SignColumn", { bg = "#000000" })
+      -- vim.api.nvim_set_hl(0, "Normal", { bg = "#000000" })
+      -- vim.api.nvim_set_hl(0, "NormalNC", { bg = "#000000" })
+      -- vim.api.nvim_set_hl(0, "SignColumn", { bg = "#000000" })
     '';
   };
 }
